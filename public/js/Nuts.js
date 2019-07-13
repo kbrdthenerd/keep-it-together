e96873fef285dc2b493f16386587bc7b3d59c3b2
Katherine Brennan <katherinepbrennan@gmail.com>
2019-07-13 10:03:50 -0500

    falling nuts

--- public/js/Nuts.js
+++ public/js/Nuts.js
@@ -8,6 +8,7 @@ class Nuts {
     create() {
         this.group = this.scene.physics.add.group({})
         this.add(250, 250)
+        this.add(200, 200)
         this.add(100, 100)
     }
 
@@ -18,26 +19,37 @@ class Nuts {
         nut.spinning = false
 
         nut.setInteractive().on('pointerover', () => {
-                nut.body.angularVelocity = 100
-                nut.spinning = true
+                if(!nut.falling) {
+                    nut.body.angularVelocity = 100
+                    nut.spinning = true
+                }
             }
 
         );
 
         nut.setInteractive().on('pointerout', () => {
-            nut.body.angularVelocity = -100
-            nut.spinning = false
+            if(!nut.falling) {
+                nut.body.angularVelocity = -100
+                nut.spinning = false
+            }
         });
     }
 
     update() {
         Phaser.Actions.Call(this.group.getChildren(), (nut) => {
-            if(!nut.spinning) {
-                let newHeight = nut.body.height + 0.05
+            if(nut.displayWidth > 100) {
+                nut.falling = true
+                nut.body.angularVelocity = 0
+                let newHeight = nut.body.height + 50
                 nut.setDisplaySize(newHeight * 1.15, newHeight)
             } else {
-                let newHeight = nut.body.height - 0.1
-                nut.setDisplaySize(newHeight * 1.15, newHeight)
+                if(!nut.spinning) {
+                    let newHeight = nut.body.height + 0.05
+                    nut.setDisplaySize(newHeight * 1.15, newHeight)
+                } else {
+                    let newHeight = nut.body.height - 0.1
+                    nut.setDisplaySize(newHeight * 1.15, newHeight)
+                }
             }
         })
     }
